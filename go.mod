02f3fa5119ce0c579074817a240d1d0de5e42590
熊挺 <1018589158@qq.com>
2020-12-22 08:08:51 +0800

    new

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module Graph
 
 go 1.13
+
+require github.com/emirpasic/gods v1.12.0
--- graph.go
+++ graph.go
@@ -4,6 +4,9 @@ import (
 	"Graph/block"
 	"errors"
 	"fmt"
+	"github.com/emirpasic/gods/sets/hashset"
+	"github.com/emirpasic/gods/sets/linkedhashset"
+	"github.com/emirpasic/gods/stacks/arraystack"
 )
 
 
@@ -16,24 +19,26 @@ type IGraph interface {
 }
 
 type Graph struct {
-	nodeList []interface{}
-	nodeIndexMap map[interface{}]int
-	edgeMatrix [][]int
-	root interface{}
+	nodeList   []interface{}
+	lookup     map[interface{}]int
+	nodeMap    map[interface{}][]interface{}
+	indegreeMap map[interface{}][]interface{}
+	root       interface{}
 }
 
 func New() *Graph{
 	return &Graph{
-		nodeList:  []interface{}{},
-		nodeIndexMap: make(map[interface{}]int),
-		edgeMatrix: [][]int{},
+		nodeList:   []interface{}{},
+		lookup:     make(map[interface{}]int),
+		nodeMap:    make(map[interface{}][]interface{}),
+		indegreeMap: make(map[interface{}][]interface{}),
 	}
 }
 
 func (G *Graph)HaveNode(node interface{}) (error,bool){
-	_,ok := G.nodeIndexMap[node]
+	_,ok := G.lookup[node]
 	if !ok{
-		return errors.New(fmt.Sprintf("nodeIndexMap cannot find such %v",node) ),ok
+		return errors.New(fmt.Sprintf("lookup cannot find such %v",node) ),ok
 	}
 
 	return nil,ok
@@ -48,7 +53,7 @@ func (G *Graph)AddNode(node interface{}) error{
 	}
 
 	G.nodeList = append(G.nodeList,node)
-	G.nodeIndexMap[node] = len(G.nodeList) -1
+	G.lookup[node] = len(G.nodeList) -1
 	return nil
 }
 
@@ -64,47 +69,7 @@ func (G *Graph)AddEdge(node1 interface{},node2 interface{}) error{
 		return  errors.New(fmt.Sprintf("node %v is same with node %v",node1,node2) )
 	}
 
-	id1 := G.nodeIndexMap[node1]
-	id2 := G.nodeIndexMap[node2]
-
-	// expand for edgeMatrix
-	var maxId int
-	if id1 > id2{
-		maxId = id1
-	}else{
-		maxId = id2
-	}
-
-	lenEdgeMatrix := len(G.edgeMatrix)
-	expandNum := 0
-	for ;expandNum + lenEdgeMatrix < maxId+1;{
-		if lenEdgeMatrix == 0{
-			expandNum += 1
-		}
-		expandNum += lenEdgeMatrix
-	}
-	if expandNum > 0{
-		var temp1 []int
-		for j:=0;j <lenEdgeMatrix;j++{
-			temp1 = append(temp1,0)
-		}
-
-		for i := 0; i < expandNum;i++{
-			G.edgeMatrix = append(G.edgeMatrix,temp1)
-		}
-
-		var temp2 []int
-		for j:=0;j < expandNum;j++{
-			temp2 = append(temp2,0)
-		}
-		for i := 0; i < len(G.edgeMatrix);i++{
-			G.edgeMatrix[i] = append(G.edgeMatrix[i],temp2...)
-		}
-	}
-
-	// add an edge
-	G.edgeMatrix[id1][id2] = 1
-
+	G.nodeMap[node1] = append(G.nodeMap[node1], node2)
 	return nil
 }
 
@@ -115,7 +80,6 @@ func (G *Graph)SetRoot(node interface{}) error{
 		panic(err)
 		return err
 	}
-
 	G.root = node
 	return nil
 }
@@ -126,25 +90,18 @@ func (G *Graph)Child(node interface{}) []interface{}{
 	if !ok{
 		panic(err)
 	}
-
-	var children []interface{}
-	id := G.nodeIndexMap[node]
-	for i:=0;i<len(G.edgeMatrix[id]);i++{
-		if G.edgeMatrix[id][i] >0{
-			children = append(children,G.nodeList[i])
-		}
-	}
+	children,_ := G.nodeMap[node]
 
 	return children
 }
 
 
-var usefulIdListMap map[int]interface{}
+var usefulIdList *linkedhashset.Set
 func (G *Graph)addUsefulId(children []interface{}){
 	if len(children)>0 {
 		for _,node := range children{
-			usefulIdListMap[G.nodeIndexMap[node]] = &struct{}{}
-			//usefulIdList = append(usefulIdList, G.nodeIndexMap[node])
+			usefulIdList.Add(G.lookup[node])
+			//usefulIdList = append(usefulIdList, G.lookup[node])
 			G.addUsefulId(G.Child(node))
 		}
 	}
@@ -157,17 +114,85 @@ func (G *Graph)SubGraph(node interface{}){
 	if !ok{
 		panic(err)
 	}
-	usefulIdListMap = make(map[int]interface{})
-	usefulIdListMap[G.nodeIndexMap[node]] = &struct{}{}
-	//usefulIdList = []int{G.nodeIndexMap[node] }
 
-	G.addUsefulId(G.Child(node))
-	fmt.Printf("%v\n",usefulIdListMap)
+	L := hashset.New()
+
+	stack := arraystack.New()
+	stack.Push(node)
+	var tmpNode interface{}
+	for !stack.Empty(){
+		tmpNode, _ = stack.Pop()
+		L.Add(tmpNode)
+		if len(G.nodeMap[tmpNode])>0{
+			for _,node := range G.nodeMap[tmpNode]{
+				stack.Push(node)
+			}
+		}
+	}
+
+	for _,node := range G.nodeList{
+		if !L.Contains(node){
+			delete(G.lookup,node)
+		}
+	}
+	G.nodeList = L.Values()
+	for key,node := range G.nodeList{
+		G.lookup[node] = key
+	}
+	for key,nodes := range G.nodeMap{
+		for _,node := range nodes {
+			G.indegreeMap[node] = append(G.indegreeMap[node], key)
+		}
+	}
+	G.indegreeMap[node] = []interface{}{}
+
+	//fmt.Printf("%v",G.indegreeMap)
+	G.root = node
 }
 
-func (G *Graph)LogicSort(){
-
+func contain(v []interface{},i interface{}) int  {
+	for key,value := range v{
+		if value==i{
+			return key
+		}
+	}
+	return -1
 }
+
+
+func (G *Graph)LogicSort( cmp func(interface{},interface{}) int) []interface{}{
+	L := linkedhashset.New() // result set
+	S := linkedhashset.New() // node set that in-degree = 0
+	S.Add(G.root)
+
+
+	for S.Size()>0 {
+		node := S.Values()[0]
+		L.Add(node)
+		S.Remove(node)
+
+		// add items no in-degree to S except in L
+		//遍历map中的key
+		for key,value := range G.indegreeMap{
+			if !L.Contains(key){
+				co := contain(value,node)
+				if co != -1{
+					G.indegreeMap[key] = append(G.indegreeMap[key][0:co],G.indegreeMap[key][co+1:]... )
+				}
+				if len(G.indegreeMap[key]) == 0{
+					S.Add(key)
+				}
+			}
+		}
+
+
+	}
+
+	fmt.Printf("%v",L.Values())
+
+	return L.Values()
+}
+
 
 func main() {
 	block1 := block.New("a")
@@ -195,7 +220,13 @@ func main() {
 	// graph.edgeMatrix[0][0] = false
 
 	// println(graph.edgeMatrix[0][0])
-	graph.SubGraph(block5)
+	graph.SubGraph(block1)
+	graph.LogicSort(func(i interface{}, i2 interface{}) int {
+		return 1
+	})
+	//graph.LogicSort( func(block1 interface{},block2 interface{}) int{ return 1} )
+
+	//fmt.Printf("%v,%v,%v\n",graph.nodeList,graph.lookup[graph.nodeList[1]],graph.edgeMatrix)
 	//fmt.Printf("%v\n",graph.edgeMatrix)
 	//println(graph.nodeList[0].(*block.Block).GetName())
 	//fmt.Printf("%v\n",graph.Child(block1)[1].(*block.Block))
